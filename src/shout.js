7d671da7fd07fdea531627eb90102ca3f2fc4e4b
Mattias Erming <mattias@mattiaserming.com>
2015-01-24 08:47:24 -0800

    Render template

--- src/shout.js
+++ src/shout.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var express = require("express");
 var io = require("socket.io");
+var fs = require("fs");
+var _ = require("lodash");
 var config = require("./config");
 
 module.exports = shout;
@@ -9,22 +11,39 @@ var sockets;
 
 function shout() {
   var app = express()
-		.use(serve)
+    .use(serve)
     .use(express.static("client"));
 
-	var srv = http.createServer(app);
-	srv.listen(config("port"));
+  var srv = http.createServer(app);
+  srv.listen(config("port"));
 
-	sockets = io(srv)
-	sockets.on("connect", function(s) {
-		init(s);
-	});
+  sockets = io(srv)
+  sockets.on("connect", function(s) {
+    init(s);
+  });
 };
 
 function serve(req, res, next) {
-	return next();
+  if (req.url.split("?")[0] != "/") {
+    return next();
+  }
+  var model = {
+    shout: JSON.stringify({
+      version: require("../package.json").version
+    })
+  };
+  return fs.readFile(
+    "client/index.html",
+    "utf-8",
+    function(err, file) {
+      res.end(_.template(
+        file,
+        model
+      ));
+    }
+  );
 }
 
 function init(socket) {
-	console.log("Client connect.");
+  console.log("Client connect.");
 }
