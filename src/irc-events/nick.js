b00ac6f501875a8aecf205aed9f07649f2be0138
Mattias Erming <mattias@mattiaserming.com>
2015-04-18 14:20:38 -0700

    Add nick event

--- src/irc-events/nick.js
+++ src/irc-events/nick.js
@@ -1,4 +1,51 @@
+var _ = require("lodash");
+var Msg = require("../../src/models/msg");
+
 module.exports = function(irc, client, network) {
   irc.on("nick", function(data) {
+    var nick = data["new"];
+    var prev = data.nick;
+
+    if (nick == irc.me) {
+      var chan = network.channels[0];
+      var msg = new Msg({
+        text: "You're now known as " + nick
+      });
+      chan.messages.push(msg);
+      client.emit("msg", {
+        chan: chan.id,
+        msg: msg
+      });
+      client.emit("nick", {
+        network: network.id,
+        nick: nick
+      });
+    }
+
+    network.channels.forEach(function(chan) {
+      var user = _.findWhere(chan.users, {name: prev});
+      if (!user) {
+        return;
+      } else {
+        user.name = nick;
+      }
+
+      var msg = new Msg({
+        type: Msg.Type.NICK,
+        from: prev,
+        text: nick
+      });
+      chan.messages.push(msg);
+      client.emit("msg", {
+        chan: chan.id,
+        msg: msg
+      });
+
+      client.emit("nick", {
+        chan: chan.id,
+        prev: prev,
+        nick: nick
+      });
+    });
   });
 };
--- test/irc-events/nick.js
+++ test/irc-events/nick.js
@@ -1,6 +1,58 @@
 var tape = require("tape");
+var events = require("events");
 var nick = require("../../src/irc-events/nick");
+var Chan = require("../../src/models/chan");
+var User = require("../../src/models/user");
 
 tape("nick", function(t) {
+  t.plan(5);
+
+  var irc = new events.EventEmitter();
+  irc.me = "foo";
+
+  var client = {};
+  client.emit = function(e, msg) {
+    switch (e) {
+    case "msg":
+      t.pass();
+      break;
+
+    case "nick":
+      if (typeof msg.network !== "undefined") {
+        if (msg.nick == "baz") {
+          t.pass();
+        }
+      } else {
+        if ((msg.prev == "foo" && msg.nick == "baz") || (msg.prev == "bar" && msg.nick == "foo")) {
+          t.pass();
+        }
+      }
+      break;
+    }
+  };
+
+  var network = {
+    id: 0,
+    channels: [new Chan({
+      name: "#bar",
+      users: [
+        new User({name: "foo"}),
+        new User({name: "bar"}),
+      ]
+    })]
+  };
+
+  nick(irc, client, network);
+
+  irc.emit("nick", {
+    nick: "foo",
+    "new": "baz"
+  });
+
+  irc.emit("nick", {
+    nick: "bar",
+    "new": "foo"
+  });
+
   t.end();
 });
