286be747454b4f77fdb6237963012afb7035242b
Mattias Erming <mattias@mattiaserming.com>
2015-04-18 13:12:24 -0700

    Add motd event

--- src/irc-events/motd.js
+++ src/irc-events/motd.js
@@ -1,4 +1,19 @@
+var Msg = require("../models/msg");
+
 module.exports = function(irc, client, network) {
   irc.on("motd", function(data) {
+    var motd = data.motd;
+    var chan = network.channels[0];
+    motd.forEach(function(text) {
+      var msg = new Msg({
+        type: Msg.Type.MOTD,
+        text: text
+      });
+      chan.messages.push(msg);
+      client.emit("msg", {
+        chan: chan.id,
+        msg: msg
+      });
+    });
   });
 };
--- test/irc-events/motd.js
+++ test/irc-events/motd.js
@@ -1,6 +1,37 @@
 var tape = require("tape");
+var events = require("events");
 var motd = require("../../src/irc-events/motd");
+var Chan = require("../../src/models/chan");
 
 tape("motd", function(t) {
+  t.plan(4);
+
+  var irc = new events.EventEmitter();
+  irc.me = "foo";
+
+  var client = {};
+  client.emit = function(e, msg) {
+    switch(e) {
+    case "msg":
+      if (msg.msg.text) {
+        t.pass();
+      }
+      break;
+    }
+  };
+
+  var network = {
+    id: 0,
+    channels: [new Chan({
+      name: "#bar"
+    })]
+  };
+
+  motd(irc, client, network);
+
+  irc.emit("motd", {
+    motd: ["this", "is", "the", "motd"]
+  });
+
   t.end();
 });
