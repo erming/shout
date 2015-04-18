224e7c0f4bb92c053f3db9894c7d18b951e42239
Mattias Erming <mattias@mattiaserming.com>
2015-04-18 14:33:04 -0700

    Add notice event

--- src/irc-events/notice.js
+++ src/irc-events/notice.js
@@ -1,4 +1,35 @@
+var _ = require("lodash");
+var Msg = require("../models/msg");
+
 module.exports = function(irc, client, network) {
   irc.on("notice", function(data) {
+    var to = data.to;
+    if (to.toLowerCase() == irc.me.toLowerCase()) {
+      to = data.from;
+    }
+
+    var chan = _.findWhere(
+      network.channels,
+      {name: to}
+    );
+    if (!chan) {
+      chan = network.channels[0];
+    }
+
+    var from = data.from;
+    if (data.to == "*" || data.from.indexOf(".") !== -1) {
+      from = "";
+    }
+
+    var msg = new Msg({
+      type: Msg.Type.NOTICE,
+      from: from,
+      text: data.message
+    });
+    chan.messages.push(msg);
+    client.emit("msg", {
+      chan: chan.id,
+      msg: msg
+    });
   });
 };
--- test/irc-events/notice.js
+++ test/irc-events/notice.js
@@ -1,6 +1,41 @@
 var tape = require("tape");
+var events = require("events");
 var notice = require("../../src/irc-events/notice");
+var Msg = require("../../src/models/msg");
 
 tape("notice", function(t) {
+  t.plan(1);
+
+  var irc = new events.EventEmitter();
+  irc.me = "foo";
+
+  var client = {};
+  client.emit = function(e, msg) {
+    switch(e) {
+    case "msg":
+      var type = msg.msg.type;
+      var text = msg.msg.text;
+      if (type == Msg.Type.NOTICE && text == "foo") {
+        t.pass();
+      }
+      break;
+    }
+  };
+
+  var network = {
+    id: 0,
+    channels: [{
+      messages: []
+    }]
+  };
+
+  notice(irc, client, network);
+
+  irc.emit("notice", {
+    message: "foo",
+    to: "bar",
+    from: "baz"
+  });
+
   t.end();
 });
