58b294b92be081fb954275cbe24101402018490d
Mattias Erming <mattias@mattiaserming.com>
2015-04-18 18:13:53 -0700

    Add topic event

--- src/irc-events/topic.js
+++ src/irc-events/topic.js
@@ -1,4 +1,32 @@
+var _ = require("lodash");
+var Msg = require("../models/msg");
+
 module.exports = function(irc, client, network) {
   irc.on("topic", function(data) {
+    var chan = _.findWhere(network.channels, {name: data.channel});
+    if (!chan) {
+      return;
+    }
+
+    var from = data.nick || chan.name;
+    var topic = data.topic;
+
+    chan.topic = topic;
+
+    var msg = new Msg({
+      type: Msg.Type.TOPIC,
+      from: from,
+      text: topic
+    });
+    chan.messages.push(msg),
+    client.emit("msg", {
+      chan: chan.id,
+      msg: msg
+    });
+
+    client.emit("topic", {
+      chan: chan.id,
+      topic: _.escape(topic)
+    });
   });
 };
--- test/irc-events/topic.js
+++ test/irc-events/topic.js
@@ -1,6 +1,43 @@
 var tape = require("tape");
+var events = require("events");
 var topic = require("../../src/irc-events/topic");
+var Chan = require("../../src/models/chan");
 
 tape("topic", function(t) {
+  t.plan(2);
+
+  var irc = new events.EventEmitter();
+  irc.me = "foo";
+
+  var client = {};
+  client.emit = function(e, msg) {
+    switch(e) {
+    case "msg":
+      t.pass();
+      break;
+
+    case "topic":
+      if (msg.topic == "foo") {
+        t.pass();
+      }
+      break;
+    }
+  };
+
+  var network = {
+    id: 0,
+    channels: [new Chan({
+      name: "#bar"
+    })]
+  };
+
+  topic(irc, client, network);
+
+  irc.emit("topic", {
+    channel: "#bar",
+    nick: "",
+    topic: "foo"
+  });
+
   t.end();
 });
