9b0db1d98e6dfca7303a8e82c3f763b8f30ac709
ruliane <ruliane+github@ruliane.net>
2015-09-23 14:36:29 +0200

    #268 : show seconds in interface
    Time format modified : seconds are displayed

--- client/js/libs/handlebars/tz.js
+++ client/js/libs/handlebars/tz.js
@@ -2,7 +2,7 @@ Handlebars.registerHelper(
 	"tz", function(time) {
 		if (time) {
 			var utc = moment.utc(time, "HH:mm:ss").toDate();
-			return moment(utc).format("HH:mm");
+			return moment(utc).format("HH:mm:ss");
 		} else {
 			return "";
 		}
